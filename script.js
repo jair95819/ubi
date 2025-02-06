e6f1e98a78239ce29b8a626199b0008bdbbef362
Samuel Campos <jairestebanca@gmail.com>
2025-02-06 13:42:30 -0500

    - Gestión de errores

--- script.js
+++ script.js
@@ -31,27 +31,8 @@ let retrieve_position =  (position) => {
     obtenerDireccion(lat, lng);
 }
 
-async function verificarPermisoUbicacion() {
-    try {
-        let permiso = await navigator.permissions.query({ name: "geolocation" });
-        
-        console.log(permiso.state);
-        if (permiso.state === "prompt") {
-            permiso_aviso = "Para obtener tu ubicación, permite el acceso a la misma.";
-        }
-
-        if (permiso.state === "denied") {
-            permiso_aviso = "Has denegado los permisos de ubicación. Ve a Ajustes > Privacidad > Ubicación y actívalos.";
-        }
-    } catch (error) {
-        console.error("Error verificando permisos de ubicación:", error);
-    }
-}
-
 
 window.inicializarMapa = async function() {
-
-    verificarPermisoUbicacion();
 
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(retrieve_position);
@@ -67,10 +48,11 @@ function rastrearUbicacion() {
                 console.error("Error obteniendo ubicación:", error);
                 let mensajeError = "";
                 console.log(error.code);
+                console.log(error.message);
 
                 switch (error.code) {
                     case error.PERMISSION_DENIED:
-                        mensajeError = "Permiso denegado. Habilita la ubicación en tu navegador." + " " + permiso_aviso;
+                        mensajeError = "Permiso denegado. Habilita la ubicación en tu navegador." + " " + error.message;
                         break;
                     case error.POSITION_UNAVAILABLE:
                         mensajeError = "Ubicación no disponible.";
